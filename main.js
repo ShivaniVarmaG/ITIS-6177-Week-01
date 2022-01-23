c21d5095d298a4fcd6457a63df6d765d384b23e5
ShivaniVarmaG <shivani1997.varma@gmail.com>
2022-01-23 10:08:08 -0500

    node server

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Hello World!")
+const http = require('http');
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello World!');
+}
+
+const server = http.createServer(requestListener);
+server.listen(8081);
+console.log("Hello world");
